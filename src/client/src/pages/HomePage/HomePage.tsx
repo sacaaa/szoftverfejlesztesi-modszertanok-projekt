1460d398ff3f8a19aef8e927fafcc7e74ce7a181
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-09-29 08:57:33 +0200

    edit: home page módosítás

--- src/client/src/pages/HomePage/HomePage.tsx
+++ src/client/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,34 @@
 import Navbar from "../../components/Navbar/Navbar";
-import Searchbar_textbox from "../../components/Searchbar_textbox/Searchbar_textbox";
+import { Link } from "react-router-dom"; // Importálás a Link komponenshez
+import './HomePage.css';
 
 export default function HomePage() {
-    return (<>
-        <Navbar />
-        <Searchbar_textbox />
-    </>)
+    return (
+        <>
+            <Navbar />
+            <div className="homepage">
+                <h1 className="title">EduStats</h1>
+                <div className="description">
+                    <p>
+                        Fedezze fel a tanárok és iskolák értékelésének világát, 
+                        ahol Önnek lehetősége van megosztani tapasztalatait és véleményét.
+                    </p>
+                    <p>
+                        Célunk, hogy segítsük a diákokat abban, hogy véleményükkel 
+                        hozzájáruljanak az oktatás minőségének javításához, 
+                        és hogy a tanárok folyamatos fejlődésen mehessenek 
+                        keresztül.
+                    </p>
+                </div>
+                <div className="button-container">
+                    <Link to="/teachers">
+                        <button className="search-button">Tanár keresés</button>
+                    </Link>
+                    <Link to="/schools">
+                        <button className="search-button">Iskola keresés</button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
